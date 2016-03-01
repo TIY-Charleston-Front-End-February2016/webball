ae7ba6d738c997271719af5909b25ae39919de84
Nathan Hall <nathan.mh@gmail.com>
2016-03-01 11:27:47 -0500

    we can make some tweets.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 var twitterClone = {
-  twitUrl: "https//tiny-tiny.herokuapp.com/collections/tweet/tweet/",
+  twitUrl: "https://tiny-tiny.herokuapp.com/collections/tweet-tweet/",
   init: function() {
     twitterClone.styling();
     twitterClone.events();
@@ -12,10 +12,26 @@ var twitterClone = {
 
   },
   events: function() {
-
-  }
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      console.log("i was tweetweeted");
+      var tweetMsg = $('input[type="text"]').val();
+      $('input[type="text"]').val("");
+      twitterClone.addTweet(tweetMsg);
+    })
+  },
   addTweet: function(tweet) {
-
+    $.ajax({
+      method: 'POST',
+      url: twitterClone.twitUrl,
+      data: {tweet: tweet},
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    })
   },
   getTweets: function(url) {
 
