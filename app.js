32df97392ccb8beebb258dce625571d8b4f83a79
Nathan Hall <nathan.mh@gmail.com>
2016-03-01 11:12:09 -0500

    basic app layout

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  twitterClone.init();
+});
+
+var twitterClone = {
+  twitUrl: "https//tiny-tiny.herokuapp.com/collections/tweet/tweet/",
+  init: function() {
+    twitterClone.styling();
+    twitterClone.events();
+  },
+  styling: function() {
+
+  },
+  events: function() {
+
+  }
+  addTweet: function(tweet) {
+
+  },
+  getTweets: function(url) {
+
+  },
+  updateTweet: function(tweet) {
+
+  },
+  deleteTweet: function(tweet) {
+
+  },
+  ajaxCall: function(method,url,data,id) {
+      
+  }
+}
